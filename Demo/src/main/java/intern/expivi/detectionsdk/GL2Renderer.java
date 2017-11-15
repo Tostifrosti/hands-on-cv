41d092af67c597154a5df20c3474061f347535ce
Bart Schut <bschut94@gmail.com>
2017-11-15 15:44:34 +0100

    #ADD - Cube changes colour based on cursor colliding with plane

--- Demo/src/main/java/intern/expivi/detectionsdk/GL2Renderer.java
+++ Demo/src/main/java/intern/expivi/detectionsdk/GL2Renderer.java
@@ -10,7 +10,50 @@ import javax.microedition.khronos.opengles.GL10;
 
 import intern.expivi.detectionlib.Vector;
 
+
 public class GL2Renderer implements GLSurfaceView.Renderer {
+    private final String vertexShaderUniformColor =
+                      "uniform mat4 u_MVPMatrix;      \n"        // A constant representing the combined model/view/projection matrix
+                    + "uniform vec4 u_Color;          \n"
+
+                    + "attribute vec4 a_Position;     \n"        // Per-vertex position information we will pass in.
+
+                    + "varying vec4 v_Color;          \n"        // This will be passed into the fragment shader.
+
+                    + "void main()                    \n"        // The entry point for our vertex shader.
+                    + "{                              \n"
+                    + "   v_Color = u_Color;          \n"        // Pass the color through to the fragment shader.
+                    // It will be interpolated across the triangle.
+                    + "   gl_Position = u_MVPMatrix   \n"    // gl_Position is a special variable used to store the final position.
+                    + "               * a_Position;   \n"     // Multiply the vertex by the matrix to get the final point in
+                    + "}                              \n";    // normalized screen coordinates.
+
+    private final String baseVertexShader =
+                    "uniform mat4 u_MVPMatrix;      \n"        // A constant representing the combined model/view/projection matrix.
+
+                    + "attribute vec4 a_Position;     \n"        // Per-vertex position information we will pass in.
+                    + "attribute vec4 a_Color;        \n"        // Per-vertex color information we will pass in.
+
+                    + "varying vec4 v_Color;          \n"        // This will be passed into the fragment shader.
+
+                    + "void main()                    \n"        // The entry point for our vertex shader.
+                    + "{                              \n"
+                    + "   v_Color = a_Color;          \n"        // Pass the color through to the fragment shader.
+                    // It will be interpolated across the triangle.
+                    + "   gl_Position = u_MVPMatrix   \n"    // gl_Position is a special variable used to store the final position.
+                    + "               * a_Position;   \n"     // Multiply the vertex by the matrix to get the final point in
+                    + "}                              \n";    // normalized screen coordinates.
+
+    private final String baseFragmentShader =
+            "precision mediump float;       \n"        // Set the default precision to medium. We don't need as high of a
+                    // precision in the fragment shader.
+                    + "varying vec4 v_Color;          \n"        // This is the color from the vertex shader interpolated across the
+                    // triangle per fragment.
+                    + "void main()                    \n"        // The entry point for our fragment shader.
+                    + "{                              \n"
+                    + "   gl_FragColor = v_Color;     \n"        // Pass the color directly through the pipeline.
+                    + "}                              \n";
+
     /**
      * Store the view matrix. This can be thought of as our camera. This matrix transforms world space to eye space;
      * it positions things relative to our eye.
@@ -23,10 +66,12 @@ public class GL2Renderer implements GLSurfaceView.Renderer {
     private float[] mProjectionMatrix = new float[16];
     private float[] mOrthogrpahicMatrix = new float[16];
 
-    private Shader shader;
+    private Shader baseShader, colorShader;
     public Cube mCube = new Cube();
     public Arrow mCursor = new Arrow();
+    public Plane[] mPlanes = new Plane[8];
     private String TAG = "GL2Renderer";
+    private float mAspectRatio;
 
     private int mScreenWidth;
     private int mScreenHeight;
@@ -64,7 +109,8 @@ public class GL2Renderer implements GLSurfaceView.Renderer {
         GLES20.glEnable(GLES20.GL_CULL_FACE);
         GLES20.glCullFace(GLES20.GL_BACK);
 
-        shader = new Shader();
+        baseShader = new Shader(baseVertexShader, baseFragmentShader);
+        colorShader = new Shader(vertexShaderUniformColor, baseFragmentShader);
     }
 
     @Override
@@ -76,16 +122,31 @@ public class GL2Renderer implements GLSurfaceView.Renderer {
         GLES20.glViewport(0, 0, width, height);
 
         Matrix.perspectiveM(mProjectionMatrix, 0, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
-        float aspectRatio = (float) width / (float) height;
-        Matrix.orthoM(mOrthogrpahicMatrix, 0 , -aspectRatio, aspectRatio, -1.0f, 1.0f, 0.1f, 100.0f);
+        mAspectRatio = (float) width / (float) height;
+        Matrix.orthoM(mOrthogrpahicMatrix, 0, -mAspectRatio, mAspectRatio, -1.0f, 1.0f, 0.1f, 100.0f);
+
+        mPlanes[0] = new Plane(new float[]{-(mAspectRatio*0.5f), 0.5f, -2.0f}, new float[]{1.0f, 0.0f, 0.0f, 1.0f});
+        mPlanes[1] = new Plane(new float[]{0.0f, 0.5f, -2.0f}, new float[]{0.0f, 1.0f, 0.0f, 1.0f});
+        mPlanes[2] = new Plane(new float[]{mAspectRatio*0.5f, 0.5f, -2.0f}, new float[]{0.0f, 0.0f, 1.0f, 1.0f});
+        mPlanes[3] = new Plane(new float[]{-(mAspectRatio*0.5f), 0.0f, -2.0f}, new float[]{1.0f, 1.0f, 0.0f, 1.0f});
+        mPlanes[4] = new Plane(new float[]{mAspectRatio*0.5f, 0.0f, -2.0f}, new float[]{1.0f, 0.0f, 1.0f, 1.0f});
+        mPlanes[5] = new Plane(new float[]{-(mAspectRatio*0.5f), -0.5f, -2.0f}, new float[]{0.0f, 1.0f, 1.0f, 1.0f});
+        mPlanes[6] = new Plane(new float[]{0.0f, -0.5f, -2.0f}, new float[]{1.0f, 1.0f, 1.0f, 1.0f});
+        mPlanes[7] = new Plane(new float[]{mAspectRatio*0.5f, -0.5f, -2.0f}, new float[]{0.0f, 0.0f, 0.0f, 1.0f});
     }
 
     @Override
     public void onDrawFrame(GL10 glUnused) {
         GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
 
-        mCube.Draw(shader, mViewMatrix, mProjectionMatrix);
-        mCursor.Draw(shader, mViewMatrix, mOrthogrpahicMatrix);
+        mCube.Draw(colorShader, mViewMatrix, mProjectionMatrix);
+        mCursor.Draw(baseShader, mViewMatrix, mOrthogrpahicMatrix);
+
+        for (Plane plane : mPlanes) {
+            plane.Draw(colorShader, mViewMatrix, mOrthogrpahicMatrix);
+        }
+
+
     }
 
     void UpdateCursorPosition(Vector position) {
@@ -93,8 +154,8 @@ public class GL2Renderer implements GLSurfaceView.Renderer {
             this.mScreenPosition = position;
 
             Vector clipping_space = new Vector();
-            clipping_space.x = (float) ( position.x * 2.0f - 1.0);
-            clipping_space.y = (float) ((1.0f -  position.y) * 2.0f - 1.0f);
+            clipping_space.x = (float) (position.x * 2.0f - 1.0);
+            clipping_space.y = (float) ((1.0f - position.y) * 2.0f - 1.0f);
             clipping_space.z = -1.0f;
             clipping_space.w = 1.0f;
             //Log.d(TAG, "UpdateCursorPosition: Clipping-space " + clipping_space.x + ", " + clipping_space.y + ", " + clipping_space.z + ", " + clipping_space.w);
@@ -124,6 +185,20 @@ public class GL2Renderer implements GLSurfaceView.Renderer {
             //Log.d(TAG, "UpdateCursorPosition: World-space " + mCursor.mPosition[0] + ", " + mCursor.mPosition[1]);
             //Log.d(TAG, "UpdateCursorPosition:");
             //translation[2] = outPoint[2] / outPoint[3];
+
+            for (Plane plane : mPlanes) {
+                float[] bb = {plane.mPosition[0] - (0.5f* mAspectRatio * plane.mSize), plane.mPosition[1] - (0.5f* plane.mSize), plane.mPosition[0] + (0.5f* mAspectRatio* plane.mSize), plane.mPosition[1] + (0.5f* plane.mSize)};
+
+                if(bb[0] <=  mCursor.mPosition[0]
+                && mCursor.mPosition[0] <= bb[2]
+                && bb[1] <=  mCursor.mPosition[1]
+                && mCursor.mPosition[1] <= bb[3])
+                {
+                    // Collision;
+                    mCube.mColor = plane.mColor;
+                    break;
+                }
+            }
         }
     }
 }
